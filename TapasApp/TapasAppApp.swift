b863f7a61ccea71d36dda244e831054e733ad134
Josue Ruiz <JosueRuiz.com@gmail.com>
2025-01-08 16:46:44 +0100

    Initial Commit

---
+++ TapasApp/TapasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapasAppApp.swift
+//  TapasApp
+//
+//  Created by Tardes on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TapasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
